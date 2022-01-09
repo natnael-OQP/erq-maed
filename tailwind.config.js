8c4bf0b1b5d9353483d95c257b225f91bdf984fd
papa-react <makiya112545@gmail.com>
2022-01-08 23:10:02 -1200

    add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				primary:"#191A2E",
+				secondary: "#75AEC7",
+				color3:"#D3D7DA",
+			},
+		},
+	},
+	plugins: [],
+};

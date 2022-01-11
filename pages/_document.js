e43fe830bc91b34a5d6523a500f6eeff621d702b
papa-react <makiya112545@gmail.com>
2022-01-11 08:45:22 -1200

    add lato font

--- pages/_document.js
+++ pages/_document.js
@@ -3,9 +3,22 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
 	render() {
 		return (
-			<Html lang="en" >
+			<Html lang="en">
 				<Head lang="en">
-					{/*font-family*/}
+					{/* lato font-family*/}
+					<link
+						rel="preconnect"
+						href="https://fonts.googleapis.com"
+					/>
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+						crossOrigin="true"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
+						rel="stylesheet"
+					></link>
 				</Head>
 				<body>
 					<Main />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
 	content: [
 		"./pages/**/*.{js,ts,jsx,tsx}",
@@ -5,10 +7,13 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			fontFamily: {
+				lato: ['Lato', ...defaultTheme.fontFamily.sans],
+			},
 			colors: {
-				primary:"#191A2E",
-				secondary: "#75AEC7",
-				color3:"#D3D7DA",
+				primary: "#191A2E",
+				secondary: "#43A7C8",
+				color3: "#D3D7DA",
 			},
 		},
 	},

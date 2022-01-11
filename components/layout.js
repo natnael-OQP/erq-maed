d45626a45b197f80c835b58f288783c9faa97970
papa-react <makiya112545@gmail.com>
2022-01-11 08:48:51 -1200

    remove px

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,9 @@
-import Footer from "./Footer";
-import Navbar from "./navBar";
+import Footer from "./footer";
+import Navbar from "./nav_bar";
 
 const Layout = ({children}) => {
     return (
-		<div className="max-w-screen-2xl mx-auto xl:px-10 lg:px-6 ">
+		<div className="max-w-screen-2xl mx-auto ">
 			<Navbar />
 			<main>{children}</main>
 			<Footer />

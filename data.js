2fb6030b65a1c649caae50bc6fccd3063c043363
papa-react <makiya112545@gmail.com>
2022-01-13 08:51:18 -1200

    sample data

---
+++ data.js
@@ -0,0 +1,51 @@
+export const about_us = {
+	paragraph1:`Oh to talking improve produce in limited offices fifteen
+                an. Wicket branch to answer do we. Place are decay men
+                hours tiled. If or of ye throwing friendly required.
+                Marianne interest in exertion as. Offering my branched
+                confined oh dashwood.`,
+	paragraph2:`Supported neglected met she therefore unwilling
+                discovery remainder. Way sentiments two indulgence
+                uncommonly own.`,
+	paragraph3 :`Village did removed enjoyed explain nor ham saw
+                calling talking. Securing as informed declared or
+                margaret. Joy horrible moreover man feelings own
+                shy. Request norland neither mistake for yet.
+                Between the for morning assured country believe. On
+                even feet time have an no at. Relation so in
+                confined smallest children unpacked delicate. Why
+                sir end believe uncivil respect. Always get adieus
+                nature day course for common. My little garret
+                repair to desire he esteem.Sister depend change off
+                piqued one. Contented continued any happiness
+                instantly objection yet her allowance. Use correct
+                day new brought tedious. By come this been in. Kept
+                easy or sons my it done.`
+};
+
+export const service_data = [
+	{
+		image: "/svg2.svg",
+		title: "assured country",
+		description:
+			"Between the for morning assured country believe.norland neither mistake for yet. for morning assured country",
+	},
+	{
+		image: "/svg3.svg",
+		title: "assured country",
+		description:
+			"Between the for morning assured country believe.norland neither mistake for yet. for morning assured country",
+	},
+	{
+		image: "/svg1.svg",
+		title: "assured country",
+		description:
+			"Between the for morning assured country believe.norland neither mistake for yet. for morning assured country",
+	},
+	{
+		image: "/svg0.svg",
+		title: "assured country",
+		description:
+			"Between the for morning assured country believe.norland neither mistake for yet. for morning assured country",
+	},
+];

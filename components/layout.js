a0030dda2b3894165ef9a5d5931430dcc665c65e
papa-react <makiya112545@gmail.com>
2022-01-17 08:03:07 -1200

    lazy load footer

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,30 @@
-import Footers from "./footers";
+import dynamic from "next/dynamic";
+const FootersComponent = dynamic(() => import("./footers"));
 import Navbar from "./nav_bar";
-
-const Layout = ({children}) => {
-    return (
+import useInView from "react-cool-inview";
+const Layout = ({ children }) => {
+	// lazy load
+	const { observe, inView } = useInView({
+		onEnter: ({ unobserve }) => unobserve(),
+	});
+	return (
 		<div className="max-w-screen-2xl mx-auto ">
 			<Navbar />
 			<main>{children}</main>
-			<Footers />
-			{/* copy right */}
-			<div className="bg-black text-white h-10 fy items-center justify-center">
-				<h3 className="text-color3 text-sm font-lato">
-					&copy; Copyright 2022 - All rights Reserved
-				</h3>
+			<div className="pt-10" ref={observe}>
+				{inView && (
+					<>
+						<FootersComponent />
+						<div className="bg-black text-white h-10 fy items-center justify-center">
+							<h3 className="text-color3 text-sm font-lato">
+								&copy; Copyright 2022 - All rights Reserved
+							</h3>
+						</div>
+					</>
+				)}
 			</div>
 		</div>
 	);
-}
+};
 
-export default Layout
+export default Layout;

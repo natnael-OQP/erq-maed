1d8de3cba36d5c06d9d6cfda845088f550478a4a
papa-react <makiya112545@gmail.com>
2022-01-18 07:45:05 -1200

    radio data

--- data.js
+++ data.js
@@ -1,13 +1,13 @@
 export const about_us = {
-	paragraph1:`Oh to talking improve produce in limited offices fifteen
+	paragraph1: `Oh to talking improve produce in limited offices fifteen
                 an. Wicket branch to answer do we. Place are decay men
                 hours tiled. If or of ye throwing friendly required.
                 Marianne interest in exertion as. Offering my branched
                 confined oh dashwood.`,
-	paragraph2:`Supported neglected met she therefore unwilling
+	paragraph2: `Supported neglected met she therefore unwilling
                 discovery remainder. Way sentiments two indulgence
                 uncommonly own.`,
-	paragraph3 :`Village did removed enjoyed explain nor ham saw
+	paragraph3: `Village did removed enjoyed explain nor ham saw
                 calling talking. Securing as informed declared or
                 margaret. Joy horrible moreover man feelings own
                 shy. Request norland neither mistake for yet.
@@ -20,7 +20,7 @@ export const about_us = {
                 piqued one. Contented continued any happiness
                 instantly objection yet her allowance. Use correct
                 day new brought tedious. By come this been in. Kept
-                easy or sons my it done.`
+                easy or sons my it done.`,
 };
 
 export const service_data = [
@@ -47,5 +47,40 @@ export const service_data = [
 		title: "assured country",
 		description:
 			"Between the for morning assured country believe.norland neither mistake for yet. for morning assured country",
+	},
+];
+
+export const Radio = [
+	{
+		id: "W4ZECbd06MY",
+		session: 2,
+		episode: 1,
+		title: "ipsum lorem ",
+		name: "cool lorem",
+		title: " neglected met she therefore ",
+	},
+	{
+		id: "TZ0vh0VefLM",
+		session: 2,
+		episode: 4,
+		title: "lorem ipsum",
+		name: "lorem cool",
+		title: " neglected met she therefore ",
+	},
+	{
+		id: "8FyNu15A7Gc",
+		session: 6,
+		episode: 3,
+		title: "lorem ipsum",
+		name: "lorem cool",
+		title: " she therefore  neglected met ",
+	},
+	{
+		id: "mrBjjvGh44U",
+		session: 6,
+		episode: 3,
+		title: "lorem ipsum",
+		name: "lorem cool",
+		title: " she therefore  neglected met ",
 	},
 ];

b9cf8356fe24ed8bd4c4cf6e3d8f859c070fd697
papa-react <makiya112545@gmail.com>
2022-01-20 06:13:04 -1200

    truncate

--- components/blog.js
+++ components/blog.js
@@ -2,7 +2,14 @@ import Image from "next/image";
 import { BiCommentDetail } from "react-icons/bi";
 import { AiOutlineCalendar, AiOutlineUser } from "react-icons/ai";
 
-const Blog = () => {
+const Blog = ({
+	description = "Morning assured country believe. On even feet time have an no at. Relation so in confined smallest children unpacked.",
+}) => {
+	const truncate = () => {
+		const myTruncatedString = description.substring(0, 115);
+		return myTruncatedString + ' .....';
+	};
+
 	return (
 		<div className="max-w-[310px] px-2  mx-auto overflow-hidden ">
 			<div className="max-w-[280px] h-[170px]  relative flex-grow my-3 bg-secondary  overflow-hidden rounded-lg sm:rounded-xl">
@@ -25,15 +32,16 @@ const Blog = () => {
 				</div>
 				<div className="fx space-x-1">
 					<BiCommentDetail className="blogIcon" />
-					<span className="text-xs text-gray-400 truncate">COMMENTS</span>
+					<span className="text-xs text-gray-400 truncate">
+						COMMENTS
+					</span>
 				</div>
 			</div>
 			<h2 className="text-base font-bold truncate text-primary  uppercase mt-5">
 				Lorem ipsum dolor sit.
 			</h2>
 			<p className="text-sm font-lato font-medium mt-5 w-full text-left pr-7 text-gray-500">
-				Morning assured country believe. On even feet time have an no
-				at. Relation so in confined smallest children unpacked.
+				{truncate(description)}
 			</p>
 			<button className=" mt-3 w-[120px]  h-9 text-primary space-x-1  fx justify-center border border-gray-500 bg-gray-50 shadow-sm text-sm font-semibold font-lato">
 				Read More

a93cea619f0f2edd7ab85c0e3238d7481a6f66d6
papa-react <makiya112545@gmail.com>
2022-01-24 18:23:24 -1200

    style spinner design

--- components/blog_container.js
+++ components/blog_container.js
@@ -6,39 +6,39 @@ import Spinner from "./spinner";
 const Blog_container = ({ radioCard }) => {
 	const [counter, setCounter] = useState(2);
 	// api
-	const { data, isLoading, isError } = Use_Fetch(
-		"/api/blog"
-	);
-    if (isLoading) {
-        return (
-			<>
-				<Spinner />
-				<Spinner />
-				<Spinner />
-			</>
+	const { data, isLoading, isError } = Use_Fetch("/api/blog");
+	if (isLoading) {
+		return (
+			<div className="w-[90vw] lg:w-[90%]  xl:w-[65%] mx-auto  h-full py-7  overflow-x-hidden fy">
+				<div className="w-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 ">
+					<Spinner />
+					<Spinner />
+					<Spinner />
+				</div>
+			</div>
 		);
-    }
-    const handelClick = () => {
-        setCounter(counter+9);
-    };
+	}
+	const handelClick = () => {
+		setCounter(counter + 9);
+	};
 	return (
 		<div className="w-[90vw] lg:w-[90%]  xl:w-[65%] mx-auto  h-full py-7  overflow-x-hidden fy">
 			<div className="w-full grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 ">
 				{data?.length > 0
-                    ? data.map((item, i) => {
-                        if (i <= counter) {
-                            return (
-								<Blog
-									key={item.id}
-									id={item.id}
-									title={item.title}
-									author={item.author}
-									body={item.body}
-									date={item.date}
-								/>
-							);
-                        }
-                    } )
+					? data.map((item, i) => {
+							if (i <= counter) {
+								return (
+									<Blog
+										key={item.id}
+										id={item.id}
+										title={item.title}
+										author={item.author}
+										body={item.body}
+										date={item.date}
+									/>
+								);
+							}
+					  })
 					: null}
 			</div>
 			<p
@@ -52,4 +52,3 @@ const Blog_container = ({ radioCard }) => {
 };
 
 export default Blog_container;
-

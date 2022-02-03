db04aabd4c75ae7c4f2c5b34a680cbaa6d66c2f0
papa-react <makiya112545@gmail.com>
2022-02-03 08:37:32 -1200

    style pages

--- pages/event.js
+++ pages/event.js
@@ -1,8 +1,11 @@
+import Image from "next/image";
+import Medium_event_card from "../components/medium_event_card";
 import Small_Event_Card from "../components/small_event_card";
+import Large_event_card from "../components/large_event_card";
 
 const Event = () => {
 	return (
-		<main className="w-full flex flex-col select-none pb-80 pt-3">
+		<main className="w-full flex flex-col select-none pb-16">
 			<h1 className="text-secondary text-2xl  md:text-3xl font-semibold text-center tracking-wide">
 				Events
 			</h1>
@@ -10,21 +13,21 @@ const Event = () => {
 				Supported neglected met she therefore unwilling discovery
 				remainder. Way sentiments two indulgence uncommonly own.
 			</h1>
-			<div className=" max-w-[900px] mx-auto mt-10  flex flex-col space-y-3">
-				<div className="grid grid-cols-1 sm:grid-cols-2 gap-y-6 md:grid-cols-3 gap-x-4   max-w-[900px] mx-auto ">
+			<div className=" max-w-[800px] mx-auto mt-10  flex flex-col space-y-3 md:py-4 ">
+				<div className="grid  gap-y-3 md:grid-cols-3 gap-x-4   max-w-[900px] mx-auto ">
 					<Small_Event_Card />
 					<Small_Event_Card />
 					<Small_Event_Card />
 				</div>
 				{/* section-2 */}
-				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 gap-y-1  max-w-[900px] h-[320px]  mx-auto ">
-					<div className="rounded-lg  md:col-span-1 bg-gray-50 w-[230px]">1</div>
-					<div className="rounded-lg  md:col-span-2 bg-gray-50 w-full">2</div>
+				<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-x-4 gap-y-3  max-w-[900px] h-full  md:h-[350px]   mx-auto relative overflow-hidden px-3 ">
+					<Medium_event_card />
+					<Large_event_card />
 				</div>
 				{/* section-3 */}
-				<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-4 mt-0  max-w-[900px] h-[320px]  mx-auto ">
-					<div className="rounded-lg  md:col-span-2 bg-gray-50 ">1</div>
-					<div className="rounded-lg  md:col-span-1 bg-gray-50  w-[230px]">2</div>
+				<div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 gap-x-4 gap-y-3  max-w-[900px] h-full  md:h-[350px]   mx-auto relative overflow-hidden px-3  ">
+					<Large_event_card />
+					<Medium_event_card />
 				</div>
 			</div>
 		</main>

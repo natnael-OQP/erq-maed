8fbcd0d8301ce7a9c69ecdfcfb40cfc7bc7fadaf
papa-react <makiya112545@gmail.com>
2022-02-03 20:49:06 -1200

    get data from store

--- components/small_event_card.jsx
+++ components/small_event_card.jsx
@@ -1,14 +1,40 @@
 import Image from "next/image";
+import { useEventPopup } from "../lib/store";
 
-const Small_Event_Card = ({ number }) => {
+const Small_Event_Card = ({
+	name,
+	image,
+	title,
+	description,
+	list,
+	author,
+}) => {
+	const toggle = useEventPopup((state) => state.toggle);
+	const setName = useEventPopup((state) => state.setName);
+	const setTitle = useEventPopup((state) => state.setTitle);
+	const setImage = useEventPopup((state) => state.setImage);
+	const setDescription = useEventPopup((state) => state.setDescription);
+	const setList = useEventPopup((state) => state.setList);
+
+	const clickHandler = () => {
+		setName(name);
+		setTitle(title);
+		setImage(image);
+		setDescription(description);
+		setList(list);
+		toggle();
+	};
 	return (
-		<div className="flex flex-col   min-w-[247px] h-[270px]  snap-center    rounded-lg overflow-hidden">
+		<div
+			onClick={clickHandler}
+			className="flex flex-col   min-w-[247px] h-[270px]  snap-center    rounded-lg overflow-hidden cursor-pointer"
+		>
 			<div className="w-full h-[65%] bg-secondary   overflow-hidden relative">
 				<div className=" w-full h-full opacity-50">
 					<Image
 						layout="fill"
 						objectFit="cover"
-						src="https://img.youtube.com/vi/8FyNu15A7Gc/0.jpg"
+						src={image}
 						alt="events"
 					/>
 				</div>
@@ -18,10 +44,10 @@ const Small_Event_Card = ({ number }) => {
 			</div>
 			<div className="w-full bg-gray-50 h-[35%] p-3 flex flex-col justify-center items-center relative">
 				<h5 className="text-sm text-gray-500 font-semibold text-center">
-					Removed enjoyed explain
+					{name}
 				</h5>
 				<h5 className="text-xs text-gray-400 font-semibold text-center mt-1">
-					By nathan
+					By {author}
 				</h5>
 				<div className="w-10 h-10 bg-primary absolute -top-5 left-3 rounded-full"></div>
 			</div>

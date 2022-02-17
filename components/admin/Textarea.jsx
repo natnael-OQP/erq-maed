09495dd5663899a40bcbe22cc207728414883a0f
papa-react <makiya112545@gmail.com>
2022-02-17 16:52:50 +0300

    Textarea created

---
+++ components/admin/Textarea.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Textarea = ({ textareaId }) => {
+	return (
+		<div className="w-[500px] flex flex-col space-y-8">
+			<textarea
+				rows={7}
+				className="text-base w-[500px]  px-2 rounded-md shadow-sm  bg-gray-200 text-gray-600 focus:bg-gray-50 outline-none   border-sky-400 focus:border-[2px] py-2 "
+				type="text"
+				id={textareaId}
+			/>
+		</div>
+	);
+};
+
+export default Textarea;

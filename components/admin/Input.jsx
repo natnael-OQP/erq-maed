d33f3e2fc5149ea94288af23149c2d0445ee2483
papa-react <makiya112545@gmail.com>
2022-02-17 16:51:54 +0300

    Input  component completed

---
+++ components/admin/Input.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Input = ({type,id}) => {
+	return (
+		<input
+			className="text-base w-[500px]  px-2 rounded-md shadow-sm  bg-gray-200 text-gray-600 focus:bg-gray-50 outline-none   border-sky-400 focus:border-[2px] py-2 "
+			type={type}
+			id={id}
+		/>
+	);
+};
+
+export default Input;

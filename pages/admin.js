abd24da9f7c0779082d6c59610e610d9951ae550
papa-react <makiya112545@gmail.com>
2022-02-17 21:23:09 +0300

    import all components

--- pages/admin.js
+++ pages/admin.js
@@ -5,26 +5,29 @@ import WhyChooseUsSection from "../components/admin/whyChooseUsSection";
 import AboutUsBrandInfo from "../components/admin/aboutUsBrandInfo";
 import Blog from "../components/admin/bloge";
 import Event from "../components/admin/Event";
+import Projects from "../components/admin/Projects";
+import Service from "../components/admin/Service";
+import SocialMedia from "../components/admin/SocialMedia";
 
 const Admin = () => {
 	return (
-		<div className="bg-gray-100 text-gray-700 w-full xl:px-10 mx-auto min-h-screen flex gap-x-10">
+		<div className="flex w-full min-h-screen mx-auto text-gray-700 bg-gray-100 xl:px-10 gap-x-10">
 			<Head>
 				<title>erqMaed CMS Panel</title>
 			</Head>
 			{/* Sidebar */}
 			<div className="sticky left-0 top-[15%] flex-[15%] h-[200px] shrink-0 ">
-				<ul className="w-full h-full flex flex-col  justify-center mx-auto  gap-y-2">
-					<li className="text-base font-lato text-gray-600 cursor-pointer font-semibold">
+				<ul className="flex flex-col justify-center w-full h-full mx-auto gap-y-2">
+					<li className="text-base font-semibold text-gray-600 cursor-pointer font-lato">
 						HomePage
 					</li>
-					<li className="text-base font-lato text-gray-600 cursor-pointer font-semibold">
+					<li className="text-base font-semibold text-gray-600 cursor-pointer font-lato">
 						AboutUs
 					</li>
-					<li className="text-base font-lato text-gray-600 cursor-pointer font-semibold">
+					<li className="text-base font-semibold text-gray-600 cursor-pointer font-lato">
 						Services
 					</li>
-					<li className="text-base font-lato text-gray-600 cursor-pointer font-semibold">
+					<li className="text-base font-semibold text-gray-600 cursor-pointer font-lato">
 						Projects
 					</li>
 				</ul>
@@ -34,7 +37,7 @@ const Admin = () => {
 				<h1 className="text-3xl font-semibold font-lato">CMS Editor</h1>
 				<hr className="my-12" />
 				{/* section container */}
-				<div className="bg-white pb-96 flex flex-col gap-y-2 p-5 shadow-md rounded-md">
+				<div className="flex flex-col p-5 bg-white rounded-md shadow-md pb-96 gap-y-2">
 					{/* HERO SECTION */}
 					<HeroSection />
 					{/* WHY Choose Us  */}
@@ -45,6 +48,12 @@ const Admin = () => {
 					<Blog />
 					{/* Event */}
 					<Event />
+					{/* Project */}
+					<Projects/>
+					{/* Service */}
+					<Service/>
+					{/* SocialMedia */}
+					<SocialMedia/>
 				</div>
 			</div>
 		</div>
